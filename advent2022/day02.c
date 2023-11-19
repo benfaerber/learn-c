3f6e410df18d06604feee749a8a908ed43e1c0ed
Ben Faerber <benfaerber@protonmail.com>
2023-11-18 23:13:10 -0700

    Did part 2

--- advent2022/day02.c
+++ advent2022/day02.c
@@ -38,6 +38,36 @@ int moveToScore(move m) {
     }
 }
 
+int desiredOutcome(char c) {
+    switch (c) {
+        case 'X': return MATCH_LOSE;
+        case 'Y': return MATCH_DRAW;
+        case 'Z':
+        default:
+            return MATCH_WIN;
+    }
+}
+
+move neededMove(move opponent, int outcome) {
+    if (outcome == MATCH_DRAW) {
+        return opponent;
+    }
+
+    if (outcome == MATCH_LOSE) {
+        switch (opponent) {
+            case rock: return scissors;
+            case paper: return rock;
+            case scissors: return paper;
+        }
+    }
+
+    switch (opponent) {
+        case rock: return paper;
+        case paper: return scissors;
+        case scissors: return rock;
+    }
+}
+
 int matchOutcome(move moveElf, move moveMe) {
     if (moveElf == moveMe) {
         return MATCH_DRAW;
@@ -70,9 +100,23 @@ void pt1() {
 }
 
 void pt2() {
+    FILE* file = fopen("day02.txt", "r");
+    char line[LINE_SIZE];
+    int totalScore = 0;
+    while (fgets(line, LINE_SIZE, file)) {
+        move elfMove = moveFromChar(line[0]);
+        char outcome = desiredOutcome(line[2]);
+        
+        move iPlay = neededMove(elfMove, outcome);
+        int bonus = moveToScore(iPlay);
+        int score = outcome + bonus;
+        totalScore += score;
+    }
 
+    printf("%d\n", totalScore);
 }
 
 int main(void) {
-    pt1();
+    // pt1();
+    pt2();
 }
