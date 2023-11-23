586dd6264ff64fe2df8ce0f5ebf26d3114f64fd2
Ben Faerber <benfaerber@protonmail.com>
2023-11-23 00:00:01 -0700

    Expression

---
+++ expression.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum _token {
+    plus,
+    minus,
+    multiply,
+    divide,
+} token;
+
+
+
+int main() {
+
+}
